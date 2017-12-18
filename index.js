d19b4ecd3f569ea2c69b560c31d4fc988793ff4b
Matthew Carter <matthewcarter@scalefactor.com>
2017-12-18 17:45:20 -0600

    remove script typo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ module.exports = {
     if (type === 'head' && missionDockId) {
       return "<script>\
         window.NickelledMissionDockSettings = {\
-          missionDockId: " + missionDockId + ",\
+          missionDockId: " + missionDockId + "\
         };\
         (function(){var NickelledMissionDock=window.NickelledMissionDock=NickelledMissionDock||{};NickelledMissionDock.preloadEvents=[];NickelledMissionDock.show=function(){NickelledMissionDock.preloadEvents.push('show')};NickelledMissionDock.hide=function(){NickelledMissionDock.preloadEvents.push('hide')};var loadMD=function(){var s,f;s=document.createElement('script');s.async=true;s.src='https://cdn.nickelled.com/mission-dock.min.js';f=document.getElementsByTagName('script')[0];f.parentNode.insertBefore(s,f);};loadMD();})();\
       </script>"
