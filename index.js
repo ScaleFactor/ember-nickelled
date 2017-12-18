6c76c259dd1a1a31d41f70d6bdce3d35eee8b188
Matthew Carter <matthewcarter@scalefactor.com>
2017-12-18 17:44:45 -0600

    include ember-nickelled snippet

--- index.js
+++ index.js
@@ -2,5 +2,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-nickelled'
+  name: 'ember-nickelled',
+
+  contentFor: function(type, config) {
+    var config = config['ember-nickelled'];
+    var missionDockId = config && config.missionDockId;
+
+    if (type === 'head' && missionDockId) {
+      return "<script>\
+        window.NickelledMissionDockSettings = {\
+          missionDockId: " + missionDockId + ",\
+        };\
+        (function(){var NickelledMissionDock=window.NickelledMissionDock=NickelledMissionDock||{};NickelledMissionDock.preloadEvents=[];NickelledMissionDock.show=function(){NickelledMissionDock.preloadEvents.push('show')};NickelledMissionDock.hide=function(){NickelledMissionDock.preloadEvents.push('hide')};var loadMD=function(){var s,f;s=document.createElement('script');s.async=true;s.src='https://cdn.nickelled.com/mission-dock.min.js';f=document.getElementsByTagName('script')[0];f.parentNode.insertBefore(s,f);};loadMD();})();\
+      </script>"
+    }
+  }
 };
